8e83c58d7a5b6ac36aa1bb69b30561957664d7d7
scotthi476 <52387571+scotthi476@users.noreply.github.com>
2024-03-08 16:17:00 -0800

    Updating Partner project
    callSearchMicroService.txt
    mainRatingSearchUI.py

--- partner_project/mainRatingSearchUI.py
+++ partner_project/mainRatingSearchUI.py
@@ -6,23 +6,26 @@ import sys
 import subprocess
 
 # UI takes user command
-print("Please type star if you want to start star rating recipe search or enter 2 to quit \n")
+print("Please type rating if you want to start star rating recipe search or enter 2 to quit \n")
 input1 = input()
-if (input1 == str("star")):
+if (input1 == str("rating")):
   file1 = open("callSearchMicroservice.txt", "r+")
-  file1.write("star")
+  file1.write("rating")
   file1.flush()
   file1.close()
-  time.sleep(5)
+  time.sleep(1)
 
   file1 = open("callSearchMicroservice.txt", "r+")
   data = file1.readline()
-  if (data.find("star") != -1):
-    #theproc = subprocess.Popen([sys.executable, "testRatingSearch.py"])
+  if (data.find("rating") != -1):
     theproc = subprocess.Popen([sys.executable, "ratingSearchServiceUI.py"])
     theproc.communicate()
   else:
-    print("cannot find star")
+    print("cannot find rating")
+  time.sleep(5)
+  file1.seek(0)
+  file1.truncate()
   file1.close()
+
 
 
